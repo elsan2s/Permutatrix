85ec08848396c0023567d6249c1d4b64f1bbb260
Revitte <51162772+Revitte@users.noreply.github.com>
2025-02-01 04:57:15 -0300

    Configuraçao inicial do projeto
    Feita a configuração inicial do projeto, principalmente no que se diz respeito a versao do JAVA, mudança na versão do driver de conecção com o MongoDB, configuração completa do banco, retirada do uso do driver JPA que estava atrapalhando e no momendo posto como enable a uso de cloud service.

---
+++ permutatrix-api/src/main/java/com/permutatrix/PermutatrixApplication.java
@@ -0,0 +1,13 @@
+package com.permutatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermutatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermutatrixApplication.class, args);
+	}
+
+}
---
+++ permutatrix-api/src/test/java/com/permutatrix/PermutatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.permutatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermutatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
