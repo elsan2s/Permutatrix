0e1578c4e56257d31f578e570eda073d90de9119
Revitte <51162772+Revitte@users.noreply.github.com>
2025-02-04 00:01:37 -0300

    Mds o que estou fazendo
    Juro solenemente que nao vou começar o projeto nunca ushushushs brinks

--- permutatrix-api/src/main/java/com/permutatrix/PermutatrixApplication.java
+++ permutatrix-api/src/main/java/com/permutatrix/PermutatrixApplication.java
@@ -14,6 +14,18 @@ public class PermutatrixApplication {
 		int confianca = random.nextInt(100);
 		System.out.println("Numero gerado para confianca:" + confianca);
 
+		// Mano eu to usando essa merda pra fazer uns calculos me deixa em paz
+		double pao = 60.00;
+		double leite = 100.00;
+		double cafe = 100.00;
+		double ovo = 100;
+		//Mistura e o que tava vivo e morreu (para o ovo nao se sentir ofendido)
+		double mistura = 300;
+
+		double cafeDaManha = pao + leite + cafe;
+		double almoco = (ovo + mistura)/2;
+		double janta = (ovo + mistura)/2;
+
 		// Ignora essa merda eu tava cansado começaremos amanhã... hoje e dia de missa.
 		if(confianca > 80) {
 			System.out.println("Voce e burro cara");
@@ -22,6 +34,11 @@ public class PermutatrixApplication {
 		} else {
 			System.out.println("Sabe o que deveria ter feito...");
 		}
+
+	}
+
+	public static double refeicoesMes(double almoco, double janta, double cafeDaManha){
+		return almoco+janta+cafeDaManha;
 	}
 
 }
