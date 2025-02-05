b748d819fce0146f79dd184b22586000021171b5
Revitte <51162772+Revitte@users.noreply.github.com>
2025-02-05 02:14:40 -0300

    Atualizado

--- permutatrix-api/src/main/java/com/permutatrix/PermutatrixApplication.java
+++ permutatrix-api/src/main/java/com/permutatrix/PermutatrixApplication.java
@@ -10,10 +10,6 @@ public class PermutatrixApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PermutatrixApplication.class, args);
 
-		Random random = new Random();
-		int confianca = random.nextInt(100);
-		System.out.println("Numero gerado para confianca:" + confianca);
-
 		// Mano eu to usando essa merda pra fazer uns calculos me deixa em paz
 		double pao = 60.00;
 		double leite = 100.00;
@@ -25,15 +21,6 @@ public class PermutatrixApplication {
 		double cafeDaManha = pao + leite + cafe;
 		double almoco = (ovo + mistura)/2;
 		double janta = (ovo + mistura)/2;
-
-		// Ignora essa merda eu tava cansado começaremos amanhã... hoje e dia de missa.
-		if(confianca > 80) {
-			System.out.println("Voce e burro cara");
-		} else if (confianca < 20){
-			System.out.println("Vai dormi tranquilo, vc precisa...");
-		} else {
-			System.out.println("Sabe o que deveria ter feito...");
-		}
 
 	}
 
