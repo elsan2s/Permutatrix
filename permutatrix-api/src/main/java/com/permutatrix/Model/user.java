5ee759d48f5be6a7fc8d5c7196801587615b205e
Revitte <51162772+Revitte@users.noreply.github.com>
2025-02-12 13:30:35 -0300

    Criado model nova USER
    Resolvido erro de conecao com o banco.

---
+++ permutatrix-api/src/main/java/com/permutatrix/Model/user.java
@@ -0,0 +1,79 @@
+package com.permutatrix.Model;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "usuarios")
+public class user {
+    
+    @Id
+    private String id;
+    private String nome;
+    private String email;
+    private String senha;
+    private Date dataCadastro;
+    
+    // Construtores
+    public user() {
+        this.dataCadastro = new Date();
+    }
+
+    public user(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.dataCadastro = new Date();
+    }
+
+    // Getters e Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", dataCadastro=" + dataCadastro +
+                '}';
+    }
+}
