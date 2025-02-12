3920c471273edd3e3e38820ae7feefb6d6bbeec0
Revitte <51162772+Revitte@users.noreply.github.com>
2025-02-12 12:35:49 -0300

    Commit começando a trabalhar

--- permutatrix-api/src/main/java/com/permutatrix/PermutatrixApplication.java
+++ permutatrix-api/src/main/java/com/permutatrix/PermutatrixApplication.java
@@ -2,7 +2,6 @@ package com.permutatrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Random;
 
 @SpringBootApplication
 public class PermutatrixApplication {
@@ -10,19 +9,5 @@ public class PermutatrixApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PermutatrixApplication.class, args);
 
-		// Mano eu to usando essa merda pra fazer uns calculos me deixa em paz
-		double churumino = 2;
-		double churumina = 1;
-
-		if(churumina > churumino){
-			System.out.println("Parabens Churumina vc é a Big Person");
-			churumina++;
-		} else {
-			System.out.println("Eu esperava mais dessa relaçao");
-			churumino++;
-		}
-		System.out.println("Valor de churumina: " + churumina);
-		System.out.println("Valor de churumino: " + churumino);
-		}
-
 	}
+}
