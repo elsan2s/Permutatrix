77f64757976d333884a35b1f4f5107c7238177c7
Otávio Revitte Bob Jazz <revitteotavio@gmail.com>
2025-06-21 09:39:30 -0300

    Capitulo 1 versiculo 8

---
+++ src/main/java/com/permutatrix/Model/Contabilidade.java
@@ -0,0 +1,14 @@
+package com.permutatrix.Model;
+
+public class Contabilidade {
+
+    // Classe secreta de rituais
+
+    String ritualDetrocaDeAlmas = "Uma alma por outra alma, se nao pode matar reviva alguem e se nao puder reviver entao e isso.";
+
+    String ritualDaSix = "Volte Joao Andrade, me desculpe nego veio.";
+
+    String praFicarRico = "Estude";
+
+    String praFuderumaPuta = "Pague";
+}
